d895ae3359753bbe84856bbcc2e59534024f8d2c
YinKangSheng <75596972+YinKangSheng@users.noreply.github.com>
2025-11-17 22:53:37 +0800

    Add files via upload
    utils更新

--- utils/train_data.py
+++ utils/train_data.py
@@ -55,11 +55,105 @@ def pad(x, p=2**6):
     return F.pad(x, pad, mode="constant", value=0)
 
 
+
 class TestData(data.Dataset):
+    def __init__(self, train_data_dir):
+        super().__init__()
+        train_list = train_data_dir
+        with open(train_list) as f:
+            txt = f.readlines()
+            # annotations = [line.strip() for line in txt if len(line.strip().split()[1:]) != 0]
+            annotations = [line.strip() for line in txt]
+
+        self.annotations = annotations
+        self.train_data_dir = train_data_dir
+
+    def get_images(self, index):
+        line = self.annotations[index].split()
+        image_path = line[0]
+        # print(image_path)
+        img_name = image_path.split('/')[-1]
+        # print(img_name)
+        image_name = img_name.split('.')[0]
+        # print(image_name)
+        gt_name = image_name
+        pgt_name = image_name
+
+        # --- Transform to tensor --- #
+        transform = Compose([ToTensor()])
+
+        gts = []
+        haze_img = Image.open("../../yolov3/" + 'VOCdevkit/VOC2007/JPEGImages/' + gt_name +'.jpg' )
+        gt = transform(haze_img)
+        # --- Check the channel is 3 or not --- #
+        if list(gt.shape)[0] is not 3 :
+            raise Exception('Bad image channel: {}'.format(gt_name))
+        gts.append(gt)
+
+        names = []
+        names.append(pgt_name)
+
+        return gts, names
+
+    def __getitem__(self, index):
+        res = self.get_images(index)
+        return res
+
+    def __len__(self):
+        return len(self.annotations)
+
+
+class TestImagenet(data.Dataset):
+    def __init__(self, train_data_dir):
+        super().__init__()
+        train_list = train_data_dir
+        with open(train_list) as f:
+            txt = f.readlines()
+            # annotations = [line.strip() for line in txt if len(line.strip().split()[1:]) != 0]
+            annotations = [line.strip() for line in txt]
+
+        self.annotations = annotations
+        self.train_data_dir = train_data_dir
+
+    def get_images(self, index):
+        image_path = self.annotations[index]
+        # print(image_path)
+        img_name = image_path.split('/')[-1]
+        # print(img_name)
+        image_name = img_name.split('.')[0]
+        # print(image_name)
+        gt_name = image_name
+        pgt_name = image_name
+
+        # --- Transform to tensor --- #
+        transform = Compose([ToTensor()])
+
+        gts = []
+        haze_img = Image.open("../../data/liuquan/Imagenet_val25k/" + image_path)
+        haze_img = haze_img.convert('RGB')
+        gt = transform(haze_img)
+        # --- Check the channel is 3 or not --- #
+        if list(gt.shape)[0] is not 3 :
+            raise Exception('Bad image channel: {}'.format(gt_name))
+        gts.append(gt)
+
+        names = []
+        names.append(pgt_name)
+
+        return gts, names
+
+    def __getitem__(self, index):
+        res = self.get_images(index)
+        return res
+
+    def __len__(self):
+        return len(self.annotations)
+
+class NewTestData(data.Dataset):
     def __init__(self,  img_dir):
         super().__init__()
         self.img_dir = img_dir
-        self.img_labels = [f for f in os.listdir(img_dir) if f.endswith(".jpg") or f.endswith(".png")]
+        self.img_labels = [f for f in os.listdir(img_dir) if f.endswith(".JPEG") or f.endswith(".png") or f.endswith(".jpg")]
 
     def get_images(self, index):
         img_label=self.img_labels[index]
@@ -70,9 +164,9 @@ class TestData(data.Dataset):
         transform = Compose([ToTensor()])
 
         gts = []
-        img = Image.open(img_path)
-        img = img.convert('RGB')
-        gt = transform(img)
+        haze_img = Image.open(img_path)
+        haze_img = haze_img.convert('RGB')
+        gt = transform(haze_img)
         # --- Check the channel is 3 or not --- #
         if list(gt.shape)[0] is not 3 :
             raise Exception('Bad image channel: {}'.format(image_name))
@@ -91,6 +185,17 @@ class TestData(data.Dataset):
         return len(self.img_labels)
 
 
+# DataLoader中collate_fn使用
+def dataset_collate(batch):
+    gts = []
+    pgts = []
+    for gt, pgt in batch:
+        gts.append(gt[0])
+        pgts.append(pgt[0])
+    gts = torch.from_numpy(np.array([item.numpy() for item in gts])).type(torch.FloatTensor)
+    pgts = torch.from_numpy(np.array([item.numpy() for item in pgts])).type(torch.FloatTensor)
+
+    return gts, pgts
 
 def testDataset_collate(batch):
     gts = []
